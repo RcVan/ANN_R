e78ca25ad05f4f9d6c66d4c014cead46ac8b388e
RcVan <36020166+RcVan@users.noreply.github.com>
2019-02-26 15:27:34 -0600

    Add files via upload

--- initWeights.r
+++ initWeights.r
@@ -5,7 +5,7 @@
 #######################################
 
 ## Description
-# This initWeights.r file generate weights with modifiable parameters.
+# This initWeights.r file generates weights with modifiable parameters.
 
 
 ## clear variables and set work directory
@@ -21,25 +21,33 @@ library(optimbase)
 ## code starts from here
 ########################################################
 
-## initWeights(input_size, output_size, range)
+## initWeights(input_size, output_size, range, bias_flag = TRUE)
 ## parameters: 
 # vector: range
-# numeric: input_size(without bias), output_size, 
+# numeric: input_size(without bias), output_size
+# boolean: bias_flag
 ## return:
 # matrix: weights (output_size * input_size+1)
-initWeights = function(input_size, output_size, range){
+initWeights = function(input_size, output_size, range, bias_flag = TRUE){
   
 ## get range 
   mi = range[1]
   ma = range[2]
   
+  
 ## initialization
 # remember that input is without bias 
-  weights = zeros(output_size, input_size + 1)
+# check bias flag to get weights input size
+  weights_input_size = ifelse(bias_flag, input_size+ 1, input_size)
+  
+# generate zero matrix
+  weights = zeros(output_size, weights_input_size)
+  
   
 ## random weights
   for (i in c(1:output_size))
-    weights[i,] = runif((input_size + 1), min = mi, max = ma)
+    weights[i,] = runif(weights_input_size, min = mi, max = ma)
+  
   
 ## return 
   return(weights)
